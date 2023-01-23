d86d55c5f04a311eb4636dcf9d22edee2ee9499c
Garanin Fedor <yesfedor.go@gmail.com>
2023-01-24 00:08:09 +0400

    Initial rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,34 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    semi: 'off',
+    'comma-dangle': ['warn', 'always-multiline'],
+    'no-multiple-empty-lines': 'off',
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/attribute-hyphenation': 'warn',
+    'vue/attributes-order': 'warn',
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-confusing-v-for-v-if': 'error',
+    'vue/order-in-components': 'warn',
+    'vue/prop-name-casing': 'warn',
+    'vue/no-dupe-keys': ['error', {
+      groups: [],
+    }],
+  },
 }
