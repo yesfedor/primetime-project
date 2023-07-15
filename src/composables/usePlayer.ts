c5b889cccda349864ee088c6b1952efc541f6ffb
Fedor Garanin <f.garanin@astrio.net>
2023-07-15 04:31:54 +0400

    Save

--- src/composables/usePlayer.ts
+++ src/composables/usePlayer.ts
@@ -8,18 +8,19 @@ export enum PlayerAlias {
 
 type PlayerAliasStrings = keyof typeof PlayerAlias
 
+const playerLocaleStorageKey = 'player-alias'
+const playerAliasDefault = PlayerAlias.svetacdn
+const playerAlias = ref(localStorage.getItem(playerLocaleStorageKey) ?? playerAliasDefault) as Ref<PlayerAliasStrings>
+
 export function usePlayer(kinopoiskId: Ref<string> | null) {
-	const playerLocaleStorageKey = 'player-alias'
-	const playerAliasDefault = PlayerAlias.svetacdn
-	const playerAlias = ref(localStorage.getItem(playerLocaleStorageKey) ?? playerAliasDefault) as Ref<PlayerAliasStrings>
 	const playerSrc = computed(() => {
 		if (!kinopoiskId) {
 			return ''
 		}
 		switch(playerAlias.value) {
-			case PlayerAlias.bazon:
-				return `//player.svetacdn.in/LDSZJq4uCNvY?kp_id=${kinopoiskId.value}&domain=${API_CONFIG.host}`
 			case PlayerAlias.svetacdn:
+				return `//player.svetacdn.in/LDSZJq4uCNvY?kp_id=${kinopoiskId.value}&domain=${API_CONFIG.host}`
+			case PlayerAlias.bazon:
 			default:
 				return `https://v1619875985.bazon.site/kp/${kinopoiskId.value}`
 		}
